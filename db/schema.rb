ef7030672701d11fedabbf2a0f770a3451845f11
Ashoka Finley <ashoka.finley@gmail.com>
2014-10-20 16:50:58 +0200

    added a few gems

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020103841) do
+ActiveRecord::Schema.define(version: 20141020144342) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "imgae_content_type"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "attachments", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
